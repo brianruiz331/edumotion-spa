aaceb19ad1a907304c7321e5ac39cec871453cd7
RGH-LS24-05 <RGH-LS24-05@UMD.LOCAL.EDU>
2026-09-19 14:14:48 -0500

    Mejora en el inicio de sesion y registro

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './views/Home';
+import Courses from './views/Courses';
+import Contact from './views/Contact';
+import Login from './views/Login';
+import Register from './views/Register';
+import AdminDashboard from './views/AdminDashboard';
+
+export default function App() {
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col bg-slate-50">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
